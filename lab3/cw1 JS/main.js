0f2e14e0e3dc0949d17cb243f5b618e4af63730e
MatthewCga <mateusz.cga@gmail.com>
2022-11-02 08:35:11 +0100

    lab3 p5

--- lab3/cw1 JS/main.js
+++ lab3/cw1 JS/main.js
@@ -4,21 +4,18 @@ const Fork = function() {
 };
 
 Fork.prototype.acquire = function(fn) {
-    const fork = this,
-        runFork = function(waitTime) {
-            setTimeout(function() {
-                if (fork.state === 0) {
-                    fork.state = 1;
-                    fn();
-                } else runFork(waitTime * 2);
-            }, waitTime);
-        };
+    const fork = this, runFork = function(waitTime) {
+        setTimeout(function() {
+            if (fork.state === 0) {
+                fork.state = 1;
+                fn();
+            } else runFork(waitTime * 2);
+        }, waitTime);
+    };
     runFork(1);
 };
 
-Fork.prototype.release = function() {
-    this.state = 0;
-};
+Fork.prototype.release = function() { this.state = 0; };
 
 const Philosopher = function(id, forks) {
     this.id = id;
@@ -30,7 +27,7 @@ const Philosopher = function(id, forks) {
     return this;
 };
 
-let fn1, fn2, counter, i;
+let counter, i, currentTest;
 
 Philosopher.prototype.startAsymmetric = function(count) {
     this.timer = 0;
@@ -50,77 +47,70 @@ Philosopher.prototype.startAsymmetric = function(count) {
                         );
                     });
                 });
-            else { --counter; if (counter === 0) fn2(); }
+            else { --counter; if (counter === 0) Stop(); }
         };
     setTimeout(function() { runAsymmetric(count);}, 1);
 };
 
 Philosopher.prototype.startWaiter = function(count, waiter) {
     this.timer = 0;
-    const forks = this.forks,
-        leftFork = this.leftFork,
-        rightFork = this.rightFork,
-        id = this.id,
-        myself = this,
-        runWaiter = function(count) {
-            myself.timerCounter = new Date().getTime();
-            if (count > 0)
-                waiter.requestForks(id, function () {
-                    forks[leftFork].acquire(function () {
-                        forks[rightFork].acquire(function () {
-                            myself.timer += new Date().getTime() - myself.timerCounter;
-                            setTimeout(
-                                function () {
-                                    forks[leftFork].release();
-                                    forks[rightFork].release();
-                                    waiter.returnForks(id, function () {runWaiter(count - 1);})
-                                    ;},
-                                1);
-                        });
+    const forks = this.forks, leftFork = this.leftFork, rightFork = this.rightFork, id = this.id, myself = this, runWaiter = function(count) {
+        myself.timerCounter = new Date().getTime();
+        if (count > 0)
+            waiter.requestForks(id, function () {
+                forks[leftFork].acquire(function () {
+                    forks[rightFork].acquire(function () {
+                        myself.timer += new Date().getTime() - myself.timerCounter;
+                        setTimeout(
+                            function () {
+                                forks[leftFork].release();
+                                forks[rightFork].release();
+                                waiter.returnForks(id, function () {
+                                    runWaiter(count - 1);
+                                })
+                                ;},
+                            1);
                     });
                 });
-            else {--counter; if (counter === 0) TestResults3();}
-        };
+            });
+        else {--counter; if (counter === 0) Stop();}
+    };
     setTimeout(function() {runWaiter(count);}, 1);
 };
 
 function acquireNaive(leftFork, rightFork, action) {
-    const fn = function(waitTime) {
+    const acquireFn = function(waitTime) {
         setTimeout(function() {
             if (leftFork.state === 0 && rightFork.state === 0) {
                 leftFork.state = rightFork.state = 1;
                 action();
-            } else fn(waitTime * 2);
+            } else acquireFn(waitTime * 2);
         }, waitTime);
-    }; fn(1);
+    }; acquireFn(1);
 }
 
 Philosopher.prototype.startNaive = function(count) {
     this.timer = 0;
-    const forks = this.forks,
-        leftFork = this.leftFork,
-        rightFork = this.rightFork,
-        myself = this,
-        runNaive = function(count) {
-            myself.timerCounter = new Date().getTime();
-            if (count > 0)
-                acquireNaive(forks[leftFork], forks[rightFork], function() {
-                    myself.timer += new Date().getTime() - myself.timerCounter;
-                    setTimeout(function() {
-                        forks[leftFork].release();
-                        forks[rightFork].release();
-                        runNaive(count - 1);
-                    }, 1);
-                });
-            else {--counter; if (counter === 0) fn1();}
-        }; setTimeout(function() { runNaive(count); }, 1);
+    const forks = this.forks, leftFork = this.leftFork, rightFork = this.rightFork, myself = this, runNaive = function(count) {
+        myself.timerCounter = new Date().getTime();
+        if (count > 0)
+            acquireNaive(forks[leftFork], forks[rightFork], function() {
+                myself.timer += new Date().getTime() - myself.timerCounter;
+                setTimeout(function() {
+                    forks[leftFork].release();
+                    forks[rightFork].release();
+                    runNaive(count - 1);
+                }, 1);
+            });
+        else {--counter; if (counter === 0) Stop();}
+    }; setTimeout(function() { runNaive(count); }, 1);
 };
 
 Waiter = function() {
     this.philosophersAtTable = [];
     this.waitingCallbacks = [];
     this.waitingPhilosophers = [];
-    for (let i = 0; i < PHIL_COUNT; i++) this.philosophersAtTable.push(0);
+    for (i = 0; i < PHIL_COUNT; i++) this.philosophersAtTable.push(0);
     return this;
 };
 
@@ -137,48 +127,46 @@ Waiter.prototype.tableCanAccess = function() {
     return count < 2 || neighbours;
 };
 
-Waiter.prototype.requestForks = function (philosopherId, fn) {
+Waiter.prototype.requestForks = function (id, fn) {
     if (this.tableCanAccess()) {
-        this.philosophersAtTable[philosopherId] = 1;
+        this.philosophersAtTable[id] = 1;
         setTimeout(fn);
     } else {
         this.waitingCallbacks.unshift(fn);
-        this.waitingPhilosophers.unshift(philosopherId);
+        this.waitingPhilosophers.unshift(id);
     }
 };
 
-Waiter.prototype.returnForks = function(philosopherId, cb) {
-    this.philosophersAtTable[philosopherId] = 0;
+Waiter.prototype.returnForks = function (id, fn) {
+    this.philosophersAtTable[id] = 0;
     while (this.waitingCallbacks.length > 0 && this.tableCanAccess()) {
-        const philosopherIdFromList = this.waitingPhilosophers.pop(),
-            cbFromList = this.waitingCallbacks.pop();
-        this.philosophersAtTable[philosopherIdFromList] = 1;
-        setTimeout(cbFromList);
-    }setTimeout(cb);
+        const pid = this.waitingPhilosophers.pop(), poppedFn = this.waitingCallbacks.pop();
+        this.philosophersAtTable[pid] = 1;
+        setTimeout(poppedFn);
+    } setTimeout(fn);
 };
 
+function Start() {
+    if (TESTS.length === 0) return;
+    currentTest = TESTS.shift();
+    counter = PHIL_COUNT;
+    currentTest();
+}
+
+function Test1() {for (i = 0; i < PHIL_COUNT; i++) philosophers[i].startNaive(ITER_COUNT);}
+function Test2() {for (i = 0; i < PHIL_COUNT; i++) philosophers[i].startAsymmetric(ITER_COUNT);}
+function Test3() {for (i = 0; i < PHIL_COUNT; i++) philosophers[i].startWaiter(ITER_COUNT, waiter);}
+
+function Stop() { Results(); console.log(">>>> " + currentTest.name + "\n"); Start(); }
 function Results() { for (i = 0; i < PHIL_COUNT; i++) console.log(philosophers[i].timer / ITER_COUNT); }
-function TestResults1() { Results(); console.log(">>>> Test podejście Naiwne\n"); }
-function TestResults2() { Results(); console.log(">>>> Test podejście Asymetryczne\n"); }
-function TestResults3() { Results(); console.log(">>>> Test podejście z Kelnerem\n"); }
 
-const PHIL_COUNT = 5, ITER_COUNT = 10;
+// SETTINGS # # # # # # # # # # # # # # # # # # # # #
+// [ Test1, Test2, Test3 ] // podejścia: Test1 - Naiwne, Test2 - Asymetryczne, Test3 - z Arbitrem
+const TESTS = [ Test1, Test2, Test3 ];
+const PHIL_COUNT = 5, ITER_COUNT = 10; // ilość filozofów, ilość iteracji (posiłków)
+// SETTINGS # # # # # # # # # # # # # # # # # # # # #
+
 const forks = [], philosophers = [], waiter = new Waiter();
-
 for (i = 0; i < PHIL_COUNT; i++) forks.push(new Fork());
 for (i = 0; i < PHIL_COUNT; i++) philosophers.push(new Philosopher(i, forks));
-
-counter = PHIL_COUNT;
-for (i = 0; i < PHIL_COUNT; i++) philosophers[i].startNaive(ITER_COUNT);
-
-fn1 = function() {
-    TestResults1();
-    counter = PHIL_COUNT;
-    for (i = 0; i < PHIL_COUNT; i++) philosophers[i].startAsymmetric(ITER_COUNT);
-};
-
-fn2 = function() {
-    TestResults2();
-    counter = PHIL_COUNT;
-    for (i = 0; i < PHIL_COUNT; i++) philosophers[i].startWaiter(ITER_COUNT, waiter);
-};
+Start();
